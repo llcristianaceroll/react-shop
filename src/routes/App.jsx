0a55c9fd22269d0c2a0d573dfbd1fd2dc4728b12
Cristian Acero <llcristianaceroll@gmail.com>
2022-07-09 21:05:29 -0500

    added SentEmail

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "../containers/Layout";
+import Home from "../pages/Home";
 import Login from "../containers/Login"
 import RecoveryPassword from "../containers/RecoveryPassword";
-import Home from "../pages/Home";
+import SendEmail from '../pages/SendEmail';
 import NotFound from "../pages/NotFound";
 import '../styles/global.css'
 
@@ -15,6 +16,12 @@ const App = () => {
                 <Route exact path="/" element={<Home/>}/>
                 <Route exact path="/login" element={<Login/>}/>
                 <Route exact path="/recovery-password" element={<RecoveryPassword/>}/>
+                <Route exact path="/send-email" element={<SendEmail/>} />
+				{/* <Route exact path="/new-password" element={<NewPassword/>} />
+				<Route exact path="/account" element={<MyAccount/>} />
+				<Route exact path="/signup" element={<CreateAccount/>} />
+				<Route exact path="/checkout" element={<Checkout/>} />
+				<Route exact path="/orders" element={<Orders/>} />					 */}
                 <Route path="*" element={<NotFound/>}/>            
          </Routes>
          </Layout>
